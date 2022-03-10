3297c0620ffbf4d741a4accc1a57eb2710ae155f
Manuel Duarte <carlosd99m@gmail.com>
2022-03-10 16:10:15 -0600

    chore: project setup

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flmarkdown/src/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
