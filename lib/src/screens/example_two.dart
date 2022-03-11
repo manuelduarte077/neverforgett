94acc3f650b57119fed658abbd404459b830cfce
Manuel Duarte <carlosd99m@gmail.com>
2022-03-11 12:18:40 -0600

    feat: add markdown editor

--- lib/src/screens/example_two.dart
+++ lib/src/screens/example_two.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class ExampleTwoScreen extends StatefulWidget {
   const ExampleTwoScreen({Key? key}) : super(key: key);
@@ -10,35 +11,61 @@ class ExampleTwoScreen extends StatefulWidget {
 class _ExampleTwoScreenState extends State<ExampleTwoScreen>
     with SingleTickerProviderStateMixin {
   TabController? _controller;
+  TextEditingController? _textController;
+
+  String text = '';
 
   @override
   void initState() {
     super.initState();
     _controller = TabController(length: 2, vsync: this);
+    _textController = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
+    const TextStyle style = TextStyle(color: Colors.white);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Markdown Editor'),
+        title: const Text('Markdown Editor', style: style),
         bottom: TabBar(
           controller: _controller,
           tabs: const <Widget>[
             Tab(
-              child: Icon(Icons.edit_off_outlined),
+              text: 'Editor',
             ),
             Tab(
-              child: Icon(Icons.preview),
+              text: 'Preview',
             ),
           ],
         ),
       ),
       body: TabBarView(
         controller: _controller,
-        children: const <Widget>[
-          Icon(Icons.add),
-          Icon(Icons.poll),
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              controller: _textController,
+              decoration: const InputDecoration(
+                hintText: 'Enter some text...',
+              ),
+              onChanged: (String text) {
+                setState(() {
+                  this.text = text;
+                });
+              },
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: MarkdownBody(
+              data: text,
+              styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)),
+            ),
+          ),
         ],
       ),
     );
